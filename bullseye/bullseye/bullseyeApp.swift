36c84b58d747691bda64a68eb157f605d30ae2ee
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-02-10 11:34:26 -0500

    add project boilerplate

---
+++ bullseye/bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Andrei Dobrinski on 2022-02-10.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
