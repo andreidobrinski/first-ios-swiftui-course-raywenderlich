cad9e8ce57c7fa0c3c8d6c6805e3bbd7da47aa0c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-02-16 13:54:25 -0500

    add state for sliderValue

--- bullseye/bullseye/ContentView.swift
+++ bullseye/bullseye/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
@@ -25,7 +26,7 @@ struct ContentView: View {
             HStack {
                 Text("1")
                     .bold()
-                Slider(value: .constant(50), in: 1.0...100.0)
+                Slider(value: self.$sliderValue, in: 1.0...100.0)
                 Text("100")
                     .bold()
             }
@@ -45,7 +46,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+//        ContentView()
         ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
