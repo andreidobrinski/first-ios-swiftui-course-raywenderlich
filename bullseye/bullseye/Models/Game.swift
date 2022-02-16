a46027876f6a2aff5cbe51b1ba6b474bb2133e73
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-02-16 14:21:31 -0500

    add Game Model

---
+++ bullseye/bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  bullseye
+//
+//  Created by Andrei Dobrinski on 2022-02-16.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 42
+    var score: Int = 0
+    var round: Int = 1
+    
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+}
